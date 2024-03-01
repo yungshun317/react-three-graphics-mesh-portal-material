5d949879b1b8b6305800dd2e266624884aed678f
yungshun317 <seraph317@gmail.com>
2024-03-02 05:30:30 +0800

    [FEAT] Create a `RoundedBox` geometry with `MeshPortalMaterial`

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,6 +1,36 @@
+import {MeshPortalMaterial, RoundedBox, useGLTF, useTexture} from "@react-three/drei";
+import {useRef, useState} from "react";
+import * as THREE from "three";
+
 const Scene = () => {
+    const [active, setActive] = useState(false);
+    const meshPortalMaterialRef = useRef();
+
+    const model = useGLTF("./static/models/1.glb");
+    const texture = useTexture("./static/textures/1.png");
+
+    const doubleClickHandler = () => {
+        setActive(!active);
+    };
+
     return (
-        <></>
+        <>
+            <RoundedBox
+                args={[3, 4, 0.1]}
+                radius={0.1}
+                onDoubleClick={doubleClickHandler}
+            >
+                <MeshPortalMaterial ref={meshPortalMaterialRef}>
+                    <primitive object={model.scene} scale={0.6} position-y={0.6} />
+
+                    <mesh>
+                        { /* args = (radius, widthSegments, heightSegments) */ }
+                        <sphereGeometry args={[5, 64, 64]} />
+                        <meshBasicMaterial map={texture} side={THREE.BackSide} />
+                    </mesh>
+                </MeshPortalMaterial>
+            </RoundedBox>
+        </>
     );
 };
 
